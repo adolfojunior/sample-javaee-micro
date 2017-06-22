58fc9ee7531dc4aeff91995b65114e840d8de15c
adolfojunior <adolfojunior@gmail.com>
2017-06-22 18:27:49 -0300

    add config to create uberJar

--- src/main/java/com/github/adolfojunior/sampleweb/domain/Task.java
+++ src/main/java/com/github/adolfojunior/sampleweb/domain/Task.java
@@ -2,11 +2,14 @@ package com.github.adolfojunior.sampleweb.domain;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @NamedQueries(@NamedQuery(name = Task.TASK_GET_ALL, query = "select t from Task t"))
@@ -17,7 +20,16 @@ public class Task {
   @Id
   @GeneratedValue
   private long id;
+
+  @NotNull
+  @Size(max = 128)
   private String name;
+
+  @Size(max = 1024)
+  private String desctiption;
+
+  @NotNull
+  @Column(updatable = false)
   private LocalDateTime creationTime;
 
   public long getId() {
